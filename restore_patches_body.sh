9a359ce8be095a8ca7241b32914c3769c6a7a82e
Bian Jin chen <kenjc.bian@rock-chips.com>
2019-06-10 17:55:58 +0800

    restore_patches: fix dirs restore error in some cases.
    Change-Id: Icf7ea72128054ba51c7c0c32528b86d43f1232d7
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- restore_patches_body.sh
+++ restore_patches_body.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 #if [[ $REPO_REMOTE == rk* ]] || [[ $REPO_RREV == rk33* ]]; then
-if test -d $PATCHES_PATH/$REPO_PATH
+if test -d ${PATCHES_PATH}/${REPO_PATH}
 then
-    GIT_HEAD=`grep commit $PATCHES_PATH/$REPO_PATH/git-merge-base.txt |cut -c8-47`
-    HAVE_PATCH=`find $PATCHES_PATH/$REPO_PATH -name *.patch`
-    HAVE_DIFF=`find $PATCHES_PATH/$REPO_PATH -name *.diff`
+    GIT_HEAD=`grep commit ${PATCHES_PATH}/${REPO_PATH}/git-merge-base.txt |cut -c8-47`
+    HAVE_PATCH=`find ${PATCHES_PATH}/${REPO_PATH} -name \*.patch`
+    HAVE_DIFF=`find ${PATCHES_PATH}/${REPO_PATH} -name local_diff.diff`
     git stash && git checkout -b restore_$DATE $GIT_HEAD
-    #git branch -D restore_temp01
     if [ -n "$HAVE_PATCH" ]; then
         git am $PATCHES_PATH/$REPO_PATH/*.patch
-        #echo $HAVE_PATCH
     fi
 
     if [ -n "$HAVE_DIFF" ]; then
         git apply $PATCHES_PATH/$REPO_PATH/local_diff.diff
-        #echo $HAVE_DIFF
     fi
 
     if [ -n "$HAVE_PATCH" -o -n "$HAVE_DIFF" ]; then
