9416c343b7fb4c1781e03ce9ee8311f38f45155b
Shiki <pitatensai@163.com>
2023-10-20 10:39:59 +0800

    适配鲁班猫2

--- read_manifest/main.cpp
+++ read_manifest/main.cpp
@@ -7,21 +7,20 @@
 
 using namespace std;
 
-void printRepoName(string manifest_file, string start_with, string prefix, string endfix) {
+void printRepoName(string manifest_file, string project, string key) {
 
     ifstream fin(manifest_file.data());
     string tempStr;
     while (getline(fin, tempStr)) {
         string result;
         // line filter, only change project line
-        if (HasKeyWordInString(tempStr, "<" + start_with)) {
-            //find, convert str and write
+        if (HasKeyWordInString(tempStr, project)) {
             string node_name;
-            //find 'name' in string line
-            if (FindKeyName(tempStr, "name=\"", "\"", &node_name)) {
-                cout << prefix << node_name << endfix << endl;
+            if (!FindKeyName(tempStr, key + "=\"", "\"", &node_name)) {
+                continue;
             } else {
-                cout << "your manifest file missing path & name, please check!";
+                cout << node_name << endl;
+                break;
             }
         }
     }
@@ -29,8 +28,7 @@ void printRepoName(string manifest_file, string start_with, string prefix, strin
 
 int main(int argc, char** argv) {
 
-    string file_name, prefix = "", endfix = "";
-    string start_with = "project";
+    string file_name, key = "", project = "";
     if (argc == 1) goto FAIL_PRINT;
 
     // Parse flags, all of which start with '-'
@@ -43,21 +41,19 @@ int main(int argc, char** argv) {
         string the_rest = argv[i+1];
         if (s[1] == 'i') {
             file_name = the_rest;
-        } else if (strcmp(s, "--start_with") == 0) {
-            start_with = the_rest;
-        } else if (strcmp(s, "--prefix") == 0) {
-            prefix = the_rest;
-        } else if (strcmp(s, "--endfix") == 0) {
-            endfix = the_rest;
+        } else if (strcmp(s, "--project") == 0) {
+            project = the_rest;
+        } else if (strcmp(s, "--key") == 0) {
+            key = the_rest;
         } else {
             goto FAIL_PRINT;
         }
     }
-    printRepoName(file_name, start_with, prefix, endfix);
+    printRepoName(file_name, project, key);
     return 0;
 
     FAIL_PRINT:
     cout << "Invalid argument!" << endl;
-    cout << "use -i manifest_file.xml --start_with \"remove-project\" --prefix \"@android\" --endfix \"@end\" " << endl;
+    cout << "use -i manifest_file.xml" << endl;
     return -1;
 }
